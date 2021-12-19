171f996c8791b9973cb28fcd1213929a536ade14
Avery Crespi <averycrespi@gmail.com>
2021-12-19 13:18:57 -0800

    Finish day18

--- solutions/day18.py
+++ solutions/day18.py
@@ -1,194 +1,265 @@
 from ast import literal_eval
+from copy import deepcopy
 from dataclasses import dataclass
-from math import ceil, floor
-from typing import Optional, Tuple
 from helpers import read_input
+from math import ceil, floor
+from typing import Iterator, Optional, Tuple, Union
 
 
 @dataclass
-class SnailfishNumber:
+class SFN:
     """Represents a snailfish number."""
 
-    left: Optional["SnailfishNumber"] = None
-    right: Optional["SnailfishNumber"] = None
-    value: Optional[int] = None
-    parent: Optional["SnailfishNumber"] = None
+    left: Union["SFN", int]
+    right: Union["SFN", int]
+    parent: Optional["SFN"] = None
 
     @staticmethod
-    def from_nested_list(a: list) -> "SnailfishNumber":
+    def from_nested_list(a: list) -> "SFN":
         """Create a snailfish number from a nested list."""
         assert len(a) == 2
-        sn = SnailfishNumber(
-            left=SnailfishNumber.from_nested_list(a[0])
-            if isinstance(a[0], list)
-            else SnailfishNumber(value=a[0]),
-            right=SnailfishNumber.from_nested_list(a[1])
-            if isinstance(a[1], list)
-            else SnailfishNumber(value=a[1]),
+        sfn = SFN(
+            left=SFN.from_nested_list(a[0]) if isinstance(a[0], list) else a[0],
+            right=SFN.from_nested_list(a[1]) if isinstance(a[1], list) else a[1],
         )
-        if sn.left and sn.right:
-            sn.left.parent = sn
-            sn.right.parent = sn
-        return sn
-
-    def is_pair(self) -> bool:
-        """Check if a snailfish number is a pair."""
-        return self.left is not None and self.right is not None
-
-    def is_regular(self) -> bool:
-        """Check is a snailfish number is regular."""
-        return self.value is not None
+        if isinstance(sfn.left, SFN):
+            sfn.left.parent = sfn
+        if isinstance(sfn.right, SFN):
+            sfn.right.parent = sfn
+        return sfn
 
     def to_nested_list(self) -> list:
         """Convert a snailfish number to a nested list."""
-        assert self.left is not None and self.right is not None
         return [
-            self.left.to_nested_list() if self.left.is_pair() else self.left.value,
-            self.right.to_nested_list() if self.right.is_pair() else self.right.value,
+            self.left.to_nested_list() if isinstance(self.left, SFN) else self.left,
+            self.right.to_nested_list() if isinstance(self.right, SFN) else self.right,
         ]
 
-    def propagate_left(self):
-        """Propagate the left value to the left."""
-        curr = self
-        while True:
-            if curr.parent is None:
-                return
-            elif curr == curr.parent.right:
-                curr = curr.parent.left
-                while curr.right:
-                    curr = curr.right
-                curr.value += self.left
-                return
-            else:
-                curr = curr.parent
+    def propagate_left(self) -> "SFN":
+        """
+        Propagate a left value to the left of a snailfish number.
 
-    def propagate_right(self):
-        """Propagate the right value to the right."""
-        curr = self
+        Returns the (possible-mutated) snailfish number.
+        """
+        # Ascend tree until the node is a right child
+        assert isinstance(self.left, int)
+        up = self
         while True:
-            if curr.parent is None:
-                return
-            elif curr == curr.parent.left:
-                curr = curr.parent.right
-                while curr.left:
-                    curr = curr.left
-                curr.value += self.right
-                return
+            if up.parent is None:
+                return self  # Found root node
+            elif up == up.parent.right:
+                if isinstance(up.parent.left, int):
+                    up.parent.left += self.left
+                    return self  # Special case: sibling is regular
+                # Descend tree until the right child is regular
+                down = up.parent.left
+                while isinstance(down.right, SFN):
+                    down = down.right
+                down.right += self.left
+                return self
             else:
-                curr = curr.parent
+                up = up.parent
 
-    def explode(self) -> bool:
-        """Explode a snailfish number, returning True if the explosion succeeded."""
-        stack: list[Tuple[SnailfishNumber, int]] = [(self, 0)]
+    def propagate_right(self) -> "SFN":
+        """
+        Propagate a right value to the right of a snailfish number.
+
+        Returns the (possibly-mutated) snailfish number.
+        """
+        # Ascend tree until the node is a left child
+        assert isinstance(self.right, int)
+        up = self
+        while True:
+            if up.parent is None:
+                return self  # Found root node
+            elif up == up.parent.left:
+                if isinstance(up.parent.right, int):
+                    up.parent.right += self.right
+                    return self  # Special case: sibling is regular
+                # Descend tree until left child is regular
+                down = up.parent.right
+                while isinstance(down.left, SFN):
+                    down = down.left
+                down.left += self.right
+                return self
+            else:
+                up = up.parent
+
+    def explode(self) -> Tuple["SFN", bool]:
+        """
+        Try to explode a snailfish number.
+
+        Returns the (possibly mutated) snailfish number and whether the explosion succeeded.
+        """
+        stack: list[Tuple[SFN, int]] = [(self, 0)]
         while len(stack) > 0:
-            sn, depth = stack.pop()
+            sfn, depth = stack.pop()
             if depth == 4:
-                sn.propagate_left()
-                sn.propagate_right()
-                sn.value = 0
-                return True
-            if sn.right:
-                stack.append((sn.right, depth + 1))
-            if sn.left:
-                stack.append((sn.left, depth + 1))
-        return False
+                sfn.propagate_left()
+                sfn.propagate_right()
+                assert sfn.parent is not None
+                if sfn == sfn.parent.left:
+                    sfn.parent.left = 0
+                elif sfn == sfn.parent.right:
+                    sfn.parent.right = 0
+                else:
+                    raise ValueError(sfn.parent)
+                return self, True
+            if isinstance(sfn.right, SFN):
+                stack.append((sfn.right, depth + 1))
+            if isinstance(sfn.left, SFN):
+                stack.append((sfn.left, depth + 1))
+        return self, False
 
-    def split(self) -> bool:
-        """Split a snailfish number, returning True if the split succeeded."""
-        stack: list[SnailfishNumber] = [self]
-        while len(stack) > 0:
-            sn = stack.pop()
-            if sn.left and sn.left.value and sn.left.value >= 10:
-                sn.left = SnailfishNumber(
-                    left=floor(sn.left.value / 2),
-                    right=ceil(sn.left.value / 2),
-                    parent=sn,
-                )
-                return True
-            if isinstance(pair.right, int) and pair.right >= 10:
-                pair.right = Pair(
-                    left=floor(pair.right / 2), right=ceil(pair.right / 2), parent=pair
-                )
-                return True
-            if isinstance(pair.right, Pair):
-                stack.append(pair.right)
-            if isinstance(pair.left, Pair):
-                stack.append(pair.left)
-        return False
+    def inorder(self) -> Iterator["SFN"]:
+        """Traverse a snailfish number in-order."""
+        if isinstance(self.left, SFN):
+            for sfn in self.left.inorder():
+                yield sfn
+        yield self
+        if isinstance(self.right, SFN):
+            for sfn in self.right.inorder():
+                yield sfn
 
-    def reduce(self):
-        """Reduce a pair by repeatedly exploding and splitting."""
+    def split(self) -> Tuple["SFN", bool]:
+        """
+        Try to split a snailfish number.
+
+        Returns the (possibly mutated) snailfish number and whether the split succeeded.
+        """
+        for sfn in self.inorder():
+            if isinstance(sfn.left, int) and sfn.left >= 10:
+                sfn.left = SFN(
+                    left=floor(sfn.left / 2), right=ceil(sfn.left / 2), parent=sfn
+                )
+                return self, True
+            if isinstance(sfn.right, int) and sfn.right >= 10:
+                sfn.right = SFN(
+                    left=floor(sfn.right / 2), right=ceil(sfn.right / 2), parent=sfn
+                )
+                return self, True
+        return self, False
+
+    def reduce(self) -> "SFN":
+        """
+        Reduce a snailfish number by repeatedly exploding and splitting.
+
+        Returns the (possibly mutated) snailfish number
+        """
         while True:
-            if self.explode():
+            _, did_explode = self.explode()
+            if did_explode:
                 continue
-            if self.split():
+            _, did_split = self.split()
+            if did_split:
                 continue
-            break
+            return self
 
     def magnitude(self) -> int:
-        """Calculate the magnitude of a pair."""
+        """Calculate the magnitude of a snailfish number."""
         return (
-            3 * (self.left.magnitude() if isinstance(self.left, Pair) else self.left)
+            3 * (self.left.magnitude() if isinstance(self.left, SFN) else self.left)
         ) + (
-            2 * (self.right.magnitude() if isinstance(self.right, Pair) else self.right)
+            2 * (self.right.magnitude() if isinstance(self.right, SFN) else self.right)
         )
 
-    def __add__(self, other: "Pair") -> "Pair":
-        pair = Pair(left=self, right=other)
-        assert isinstance(pair.left, Pair) and isinstance(pair.right, Pair)
-        pair.left.parent = pair
-        pair.right.parent = pair
-        pair.reduce()
-        return pair
+    def __add__(self, other: "SFN") -> "SFN":
+        sfn = SFN(left=deepcopy(self), right=deepcopy(other))
+        assert isinstance(sfn.left, SFN) and isinstance(sfn.right, SFN)
+        sfn.left.parent = sfn
+        sfn.right.parent = sfn
+        return sfn.reduce()
 
-    def __eq__(self, other: object) -> bool:
-        return isinstance(other, Pair) and id(self) == id(other)
-
-    def __str__(self) -> str:
-        return str(self.to_nested_list())
+    def __eq__(self, other) -> bool:
+        return isinstance(other, SFN) and id(self) == id(other)
 
 
-def part1(pairs: list[Pair]) -> int:
+def test_explode():
+    data = [
+        ([[[[[9, 8], 1], 2], 3], 4], [[[[0, 9], 2], 3], 4]),
+        ([7, [6, [5, [4, [3, 2]]]]], [7, [6, [5, [7, 0]]]]),
+        ([[6, [5, [4, [3, 2]]]], 1], [[6, [5, [7, 0]]], 3]),
+        (
+            [[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]],
+            [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]],
+        ),
+        (
+            [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]],
+            [[3, [2, [8, 0]]], [9, [5, [7, 0]]]],
+        ),
+        (
+            [[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]],
+            [[[[0, 7], 4], [7, [[8, 4], 9]]], [1, 1]],
+        ),
+        (
+            [[[[0, 7], 4], [7, [[8, 4], 9]]], [1, 1]],
+            [[[[0, 7], 4], [15, [0, 13]]], [1, 1]],
+        ),
+        (
+            [[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]],
+            [[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]],
+        ),
+    ]
+    for input, expected in data:
+        output, did_explode = SFN.from_nested_list(input).explode()
+        assert did_explode and output.to_nested_list() == expected
+
+
+def test_split():
+    data = [
+        (
+            [[[[0, 7], 4], [15, [0, 13]]], [1, 1]],
+            [[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]],
+        ),
+        (
+            [[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]],
+            [[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]],
+        ),
+    ]
+    for input, expected in data:
+        output, did_split = SFN.from_nested_list(input).split()
+        assert did_split and output.to_nested_list() == expected
+
+
+def test_add():
+    data = [
+        (
+            [[[[4, 3], 4], 4], [7, [[8, 4], 9]]],
+            [1, 1],
+            [[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]],
+        )
+    ]
+    for a, b, expected in data:
+        output = SFN.from_nested_list(a) + SFN.from_nested_list(b)
+        assert output.to_nested_list() == expected
+
+
+test_explode()
+test_split()
+test_add()
+
+
+def part1(sfns: list[SFN]) -> int:
     """What is the magnitude of the final sum?"""
-    curr = pairs[0]
-    for pair in pairs[1:]:
-        print("  ", str(curr))
-        print("+ ", str(pair))
-        curr += pair
-        print("= ", str(curr))
-        print()
+    curr = sfns[0]
+    for sfn in sfns[1:]:
+        curr += sfn
     return curr.magnitude()
 
 
-def part2():
-    pass
+def part2(sfns: list[SFN]) -> int:
+    """What is the largest magnitude of any sum of two different snailfish numbers from the homework assignment?"""
+    best = 0
+    for a in sfns:
+        for b in sfns:
+            if a != b:
+                best = max(best, (a + b).magnitude())
+                best = max(best, (b + a).magnitude())
+    return best
 
 
-def test():
-    p = Pair.from_nested_list([[[[[9, 8], 1], 2], 3], 4])
-    p.explode()
-    assert p.to_nested_list() == [[[[0, 9], 2], 3], 4]
+lines = read_input(18)
+sfns = [SFN.from_nested_list(literal_eval(line)) for line in lines]
 
-    p = Pair.from_nested_list([7, [6, [5, [4, [3, 2]]]]])
-    p.explode()
-    assert p.to_nested_list() == [7, [6, [5, [7, 0]]]]
-
-    p = Pair.from_nested_list([[6, [5, [4, [3, 2]]]], 1])
-    p.explode()
-    assert p.to_nested_list() == [[6, [5, [7, 0]]], 3]
-
-    p = Pair.from_nested_list([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]])
-    p.explode()
-    assert p.to_nested_list() == [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]]
-
-    p = Pair.from_nested_list([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]])
-    p.explode()
-    assert p.to_nested_list() == [[3, [2, [8, 0]]], [9, [5, [7, 0]]]]
-
-
-lines = read_input(18, sample=True)
-numbers = [literal_eval(line) for line in lines]
-pairs = [Pair.from_nested_list(n) for n in numbers]
-
-test()
-print(part1(pairs))
+print(part1(sfns))
+print(part2(sfns))
